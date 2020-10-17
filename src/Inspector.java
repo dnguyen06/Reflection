60130161ef5e52f1607dc8dd463dec27c795808c
David Nguyen <david.nguyen2@ucalgary.ca>
2020-10-17 15:48:30 -0600

    Added all intial files for the assignment

---
+++ src/Inspector.java
@@ -0,0 +1,17 @@
+/**
+ * CPSC 501
+ * Inspector starter class
+ *
+ * @author Jonathan Hudson
+ */
+public class Inspector {
+
+    public void inspect(Object obj, boolean recursive) {
+        Class c = obj.getClass();
+        inspectClass(c, obj, recursive, 0);
+    }
+
+    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+    }
+
+}
