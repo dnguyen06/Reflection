45e3c483f5a89728eafa4452b9be71e7782896bf
David Nguyen <david.nguyen2@ucalgary.ca>
2020-10-17 18:01:27 -0600

    Added functionality to handle class name, superclass name, and interface name, recursing as needed.

--- src/Inspector.java
+++ src/Inspector.java
@@ -12,6 +12,38 @@ public class Inspector {
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+    	String className = c.getName();
+    	String tabs = getTabs(depth);
+    	System.out.println(tabs + "CLASS");
+    	System.out.println(tabs + "Class: " + className);
+    	if (c.getSuperclass() == null) {
+    		System.out.println(tabs + "Superclass: NONE");
+    	} else {
+        	System.out.println(tabs + "SUPERCLASS -> Recursively Inspect");
+        	System.out.println(tabs + "Superclass : " + c.getSuperclass().getName());
+        	inspectClass(c.getSuperclass(), obj, recursive, ++depth);
+    	}
+    	System.out.println(tabs + "INTERFACES( " + className + " )");
+    	Class[] interfaces = c.getInterfaces();
+    	if(interfaces.length == 0) {
+    		System.out.println(tabs + "Interfaces-> NONE");
+    	} else {
+    		System.out.println(tabs + "Interfaces->" + interfaces.length);
+    		for (int i = 0; i < interfaces.length; i++) {
+    			System.out.println(tabs + " INTERFACE -> Recursively Inspect");
+    			System.out.println(tabs + " " + interfaces[i].getName());
+    			inspectClass(interfaces[i], obj, recursive, ++depth);
+    		}
+    	}
+    	
+    }
+    
+    private String getTabs(int depth) {
+    	String tabs = "";
+    	for(int i = 0; i < depth; i++) {
+    		tabs += "\t";
+    	}
+    	return tabs;
     }
 
 }
