e5d95b6a694b860fe80d99b8bc67733e0f4f9b05
David Nguyen <david.nguyen2@ucalgary.ca>
2020-10-18 17:44:01 -0600

    Implemented outputs for constructors

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.*;
+import java.lang.Class;
+import java.lang.Object;
+
 /**
  * CPSC 501
  * Inspector starter class
@@ -16,6 +20,7 @@ public class Inspector {
     	String tabs = getTabs(depth);
     	System.out.println(tabs + "CLASS");
     	System.out.println(tabs + "Class: " + className);
+    	// DEBUG: System.out.println(tabs + "Superclass: " + c.getSuperclass() + " Current Class: " + c.getName());
     	if (c.getSuperclass() == null) {
     		System.out.println(tabs + "Superclass: NONE");
     	} else {
@@ -29,6 +34,7 @@ public class Inspector {
     	if(interfaces.length == 0) {
     		System.out.println(tabs + "Interfaces-> NONE");
     	} else {
+    		System.out.println(tabs + "Interfaces->");
     		for (int i = 0; i < interfaces.length; i++) {
     			System.out.println(tabs + " INTERFACE -> Recursively Inspect");
     			System.out.println(tabs + " " + interfaces[i].getName());
@@ -36,6 +42,67 @@ public class Inspector {
     			depth--;
     		}
     	}
+    	
+    	System.out.println(tabs + "CONSTRUCTORS( " + className + " )");
+    	Constructor[] constructors = c.getDeclaredConstructors();
+    	if(constructors.length == 0) {
+    		System.out.println(tabs + "Constructor-> NONE");
+    	} else {
+    		System.out.println(tabs + "Constructors->");
+    		for (int i = 0; i < constructors.length; i++) {
+    			System.out.println(tabs + " CONSTRUCTOR");
+    			System.out.println(tabs + "  " + "Name: " + constructors[i].getName());
+    			Class [] parameters = constructors[i].getParameterTypes();
+    			System.out.println(tabs + "  " + "Parameter types:");
+    			if (parameters.length != 0) {
+    				for (int j = 0; j < parameters.length; j++) {
+    					System.out.println(tabs + "  " + parameters[j]);
+    				}
+    			}
+    			int modifier = constructors[i].getModifiers();
+    			if (modifier == 0) {
+    				System.out.println(tabs + "  " + "Modifiers: NONE");
+    			} else {
+    				System.out.println(tabs +  "  " + "Modifiers: " + Modifier.toString(modifier));
+    			}
+    		}
+    		
+    	
+    	}
+    	
+    	System.out.println(tabs + "METHODS( " + className + " )");
+    	Method[] methods = c.getDeclaredMethods();
+    	if(methods.length == 0) {
+    		System.out.println(tabs + "Methods-> NONE");
+    	} else {
+    		System.out.println(tabs + "Methods->");
+    		for (int i = 0; i < methods.length; i++) {
+    			System.out.println(tabs + " METHOD");
+    			System.out.println(tabs + "  " + "Name: " + methods[i].getName());
+    			Class[] exceptions = methods[i].getExceptionTypes();
+    			if(exceptions.length == 0) {
+    				System.out.println(tabs + "  " + "Exceptions-> NONE");
+    			} else {
+    				System.out.println(tabs + "  " + "Exceptions->");
+    				for(int j = 0; j < exceptions.length; j++) {
+    					System.out.println(tabs + "  " + exceptions[j]);
+    				}
+    				
+    			}
+    			Class[] parameters = methods[i].getParameterTypes();
+    			if(parameters.length == 0) {
+    				System.out.println(tabs + "  " + "Parameter types-> NONE");
+    			} else {
+    				System.out.println(tabs + "  " + "Parameter types->");
+    				for(int j = 0; j < parameters.length; j++) {
+    					System.out.println(tabs + "  " + parameters[j]);
+    				}
+    			}
+    			System.out.println(tabs + "  " + "Return type: " + methods[i].getReturnType());
+    			
+    		}
+    	}
+    	
     	
     }
     
